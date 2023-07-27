3c59ea331398ed2bfe6e53c1434d2585c5b81fa1
Miral Savera <miralsavera2702@gmail.com>
2023-07-27 17:59:51 +0530

    admin-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-
+import Breadcrumbs from './components/Breadcrumbs';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <Navbar />
+                <Sidebar />
+                <div className="app-content content">
+                    <div className="content-wrapper">
+                        <div className="content-wrapper-before"></div>
+                        <Breadcrumbs />
+                        <div class="content-body">
+                        <section id="chartjs-bar-charts">
+                        </section>
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        </>
+    );
 }
-
 export default App;
---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Breadcrumbs() {
+    return (
+        <div>
+            <div className="content-header row">
+                <div className="content-header-left col-md-4 col-12 mb-2">
+                    <h3 className="content-header-title">Charts</h3>
+                </div>
+                <div className="content-header-right col-md-8 col-12">
+                    <div className="breadcrumbs-top float-md-right">
+                        <div className="breadcrumb-wrapper mr-1">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="index.html">Home</a>
+                                </li>
+                                <li className="breadcrumb-item active">Charts
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Breadcrumbs
