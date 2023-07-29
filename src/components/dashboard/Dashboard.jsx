76de6883a39b824eaaa2a6aa4084e23a7922e0b7
Miral Savera <miralsavera2702@gmail.com>
2023-07-30 00:18:30 +0530

    DEMO : Get data From API Using Redux Feature

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch,useSelector } from "react-redux";
+import { fetchEvents } from "../../redux/slice/event";
+import { useLocation } from 'react-router-dom';
 
 function Dashboard() {
+
+    let location = useLocation();
+    const dispatch = useDispatch(); 
+    const state = useSelector((state) => state);
+    useEffect( () => {
+        dispatch(fetchEvents());
+    },[location]);
+
+    if(state.event.isLoading){
+        return <h3>Loading.....</h3>;
+    }
+
     return (
         <>
             <section id="chartjs-bar-charts">
@@ -37,6 +52,25 @@ function Dashboard() {
                             </div>
                             <div className="card-content collapse show">
                                 <div className="card-body">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="chartjs-bar-charts">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4 className="card-title">Organized Events</h4>
+                                <a href="/" className="heading-elements-toggle"><i className="la la-ellipsis-v font-medium-3"></i></a>
+                            </div>
+                            <div className="card-content collapse show">
+                                <div className="card-body">
+                                    {state.event.data && state.event.data.map((e) => <li>{e.title}</li>)}
                                 </div>
                             </div>
                         </div>
