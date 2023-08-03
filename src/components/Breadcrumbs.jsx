e6b94f730e59a04a1a565950d5befd4aba5cd25f
pritesh-52 <priteshbhatiya52@gmail.com>
2023-08-04 00:52:18 +0530

    loginpage commit

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function Breadcrumbs() {
     return (
         <div>
-            <div class="content-header row">
-                <div class="content-header-left col-md-4 col-12 mb-2">
-                    <h3 class="content-header-title">Dashboard</h3>
+            <div className="content-header row">
+                <div className="content-header-left col-md-4 col-12 mb-2">
+                    <h3 className="content-header-title">Charts</h3>
                 </div>
-                <div class="content-header-right col-md-8 col-12">
-                    <div class="breadcrumbs-top float-md-right">
-                        <div class="breadcrumb-wrapper mr-1">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a>
+                <div className="content-header-right col-md-8 col-12">
+                    <div className="breadcrumbs-top float-md-right">
+                        <div className="breadcrumb-wrapper mr-1">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="index.html">Home</a>
                                 </li>
-                                <li class="breadcrumb-item active">Charts
+                                <li className="breadcrumb-item active">Charts
                                 </li>
                             </ol>
                         </div>
