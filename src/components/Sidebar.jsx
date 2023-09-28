c801e4b2ae59fcd1aeb5d16e30d0b2c8e04234d3
Miral Savera <miralsavera2702@gmail.com>
2023-09-28 22:36:06 +0530

    Feat : Sidebar Icons changed to font awesome

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,30 +18,30 @@ function Sidebar() {
                 <div className="main-menu-content">
                     <ul className="navigation">
                         <li className={`nav-link ${location.pathname === "/admin/dashboard" ? "active" : ""} `}>
-                            <Link to="/admin/dashboard"><i className="ft-home"></i><span className="menu-title" data-i18n="">Dashboard</span></Link>
+                            <Link to="/admin/dashboard"><i class="fas fa-home"></i><span className="menu-title" data-i18n="">Dashboard</span></Link>
                         </li>
                         <li className={`nav-link ${location.pathname === "/admin/department" ? "active" : ""} `}>
-                            <Link to="/admin/department"><i className="ft-cpu"></i><span className="menu-title" data-i18n="">Department</span></Link>
+                            <Link to="/admin/department"><i class="fas fa-building"></i><span className="menu-title" data-i18n="">Department</span></Link>
                         </li>
                         <li className={`nav-link ${location.pathname === "/admin/course" ? "active" : ""} `}>
-                            <Link to="/admin/course"><i className="ft-life-buoy"></i><span className="menu-title" data-i18n="">Course</span></Link>
+                            <Link to="/admin/course"><i class="fas fa-briefcase"></i><span className="menu-title" data-i18n="">Course</span></Link>
                         </li>
                         <li className={`nav-link ${location.pathname === "/admin/faculty" ? "active" : ""} `}>
-                            <Link to="/admin/faculty"><i className="ft-users"></i><span className="menu-title" data-i18n="">Faculty</span></Link>
+                            <Link to="/admin/faculty"><i class="fas fa-chalkboard-teacher"></i><span className="menu-title" data-i18n="">Faculty</span></Link>
                         </li>
                         <li className={`nav-link ${location.pathname === "/admin/eventtype" ? "active" : ""} `}>
-                            <Link to="/admin/eventtype"><i className="ft-users"></i><span className="menu-title" data-i18n="">Events Type</span></Link>
+                            <Link to="/admin/eventtype"><i class="fas fa-th-large"></i><span className="menu-title" data-i18n="">Events Type</span></Link>
                         </li>
 
                         <li className={`nav-link ${location.pathname === "/admin/eventlocation" ? "active" : ""} `}>
-                            <Link to="/admin/eventlocation"><i className="ft-users"></i><span className="menu-title" data-i18n="">Events Location</span></Link>
+                            <Link to="/admin/eventlocation"><i class="fas fa-map-marker-alt"></i><span className="menu-title" data-i18n="">Events Location</span></Link>
                         </li>
                         <li className={`nav-link ${location.pathname === "/admin/notification" ? "active" : ""} `}>
-                            <Link to="/admin/notification"><i className="ft-users"></i><span className="menu-title" data-i18n="">Notification</span></Link>
+                            <Link to="/admin/notification"><i class="fas fa-bell"></i><span className="menu-title" data-i18n="">Notification</span></Link>
                         </li>
 
                         <li className={`nav-link ${location.pathname === "/admin/settings" ? "active" : ""} `}>
-                            <Link to="/admin/settings"><i className="ft-users"></i><span className="menu-title" data-i18n="">Settings</span></Link>
+                            <Link to="/admin/settings"><i class="fas fa-cog"></i><span className="menu-title" data-i18n="">Settings</span></Link>
                         </li>
                         
                     </ul>
